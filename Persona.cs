b469230dd7d391436d3499d266d2bf3a8476167c
Odalis Rea <68356142+Odalis26@users.noreply.github.com>
2021-07-01 21:53:04 -0500

    añadir archivos

---
+++ Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AppPersona
+{
+    internal class Persona
+    {
+
+        public string Nombre { get { return _nombre1; } set { _nombre1= value; } }
+
+        public string Apellido { get; set; }
+        public string estado1;
+        private string _nombre1;
+        public string GetEstado()
+        {
+            return estado1;
+        }
+        public void SetEstado(string estado)
+        {
+            this.estado1= estado;
+        }
+        public string Saludar()
+        {
+            return $"Hola me llamo: {this.Nombre}, espero que te encuentres bien ";
+        }
+
+        public void Dormir()
+        {
+            estado1 = "Estoy durmirndo";
+            Console.WriteLine(estado1);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AppPersona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Odalis";
+            persona.Apellido ="Rea";
+            persona.Saludar(); 
+            Console.WriteLine("Nombre:" + persona.Nombre);
+            Console.WriteLine("Apellido:" + persona.Apellido);
+            Console.WriteLine("Saludar: " + persona.Saludar());
+            persona.Dormir();
+        }
+    }
+}
