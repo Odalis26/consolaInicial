11337bd81605c240dc60a4de8d47f6731a93ec44
Odalis Rea <68356142+Odalis26@users.noreply.github.com>
2021-07-08 15:26:24 -0500

    añadir archivos

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class2 elemento = new Class2();
+            elemento.nombre = "Odalis";
+            Console.WriteLine($"Nombre:  {elemento.nombre}" );
+        }
+
+
+    }
+}
